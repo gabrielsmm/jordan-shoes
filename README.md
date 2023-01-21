f2ad2699f8bb45e570b3f06fdb688c44bed0abcb
gabrielsmm <gabrielsmm251@gmail.com>
2023-01-21 12:10:50 -0300

    Projeto criado

---
+++ README.md
@@ -0,0 +1,13 @@
+# Jordan Shoes
+
+# 🚀 Tecnologias
+
+Projeto desenvolvido com as seguintes tecnologias:
+
+HTML <br>
+CSS <br>
+JavaScript <br>
+
+# 💻 Projeto
+
+Projeto desenvolvido com o intuito de colocar em prática conhecimentos acerca do desenvolvimento Web.
---
+++ js/script.js
@@ -0,0 +1,14 @@
+var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 30,
+    showCursor: false
+});
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '50px',
+    duration: 2000,
+    reset: true
+});
+
+ScrollReveal().reveal('.card-anuncio', { delay: 150 });
